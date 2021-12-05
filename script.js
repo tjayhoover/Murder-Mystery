990c17982a6376b229d4a621ecacefe91193ea1f
Tyler Hoover <80370906+tjayhoover@users.noreply.github.com>
2021-12-05 13:37:57 -0500

    The rooms now expand when you click on them

--- script.js
+++ script.js
@@ -1,19 +1,87 @@
 $(document).ready(function () {
 
-  $("#master_bedroom").click(function () {
-    $(this).css({
-      'postion': 'absolute', 'top': '100px', 'left': '100px', 'z-index': '100',
-      'width': '500px', 'float': 'none', 'height': '500px', 'background-color': 'green'
-    });
+// These functions expand the rooms when you click on them
+  $('#office').click(function () {
+    $(this).toggleClass('cur');
+    $('#living_room').toggle();
+    $('#master_bedroom').toggle();
+    $('#guest_room').toggle();
+    $('#kitchen').toggle();
+    $('#dining_room').toggle();
+    $('#bathroom').toggle();
+    return false;
   });
 
-  $('#office').click(function () {
-    $(this).toggleClass('cur', 'room');
+  $('#living_room').click(function () {
+    $(this).toggleClass('cur');
+    $('#office').toggle();
+    $('#master_bedroom').toggle();
+    $('#guest_room').toggle();
+    $('#kitchen').toggle();
+    $('#dining_room').toggle();
+    $('#bathroom').toggle();
+    return false;
+  });
+  
+  $('#master_bedroom').click(function () {
+    $(this).toggleClass('cur');
+    $('#office').toggle();
+    $('#living_room').toggle();
+    $('#guest_room').toggle();
+    $('#kitchen').toggle();
+    $('#dining_room').toggle();
+    $('#bathroom').toggle();
+    return false;
+  });
+  
+  $('#guest_room').click(function () {
+    $(this).toggleClass('cur');
+    $('#office').toggle();
+    $('#master_bedroom').toggle();
+    $('#living_room').toggle();
+    $('#kitchen').toggle();
+    $('#dining_room').toggle();
+    $('#bathroom').toggle();
+    return false;
+  });
+  
+  $('#kitchen').click(function () {
+    $(this).toggleClass('cur');
+    $('#office').toggle();
+    $('#master_bedroom').toggle();
+    $('#guest_room').toggle();
+    $('#living_room').toggle();
+    $('#dining_room').toggle();
+    $('#bathroom').toggle();
+    return false;
+  });
+  
+  $('#dining_room').click(function () {
+    $(this).toggleClass('cur');
+    $('#office').toggle();
+    $('#master_bedroom').toggle();
+    $('#guest_room').toggle();
+    $('#kitchen').toggle();
+    $('#living_room').toggle();
+    $('#bathroom').toggle();
+    return false;
+  });
+  
+  $('#bathroom').click(function () {
+    $(this).toggleClass('cur');
+    $('#office').toggle();
+    $('#master_bedroom').toggle();
+    $('#guest_room').toggle();
+    $('#kitchen').toggle();
+    $('#dining_room').toggle();
+    $('#living_room').toggle();
     return false;
   });
 
 
-
+  // These functions allow the user to flip through the story
+  // Breana coded this up, I (Tyler) made a few slight changes to fix the issue with being able to go
+  // from the first to the last without going through the whole story
   $(".one").click(function () {
     $(this).hide();
     $(".two").show();
@@ -58,6 +126,5 @@ $(document).ready(function () {
     $(this).hide();
     $(".three").show();
   });
-
 
 }); //end doc.ready
