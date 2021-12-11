a56553083de9b5edc2d7d84acc45aa043520d2f2
Tyler Hoover <80370906+tjayhoover@users.noreply.github.com>
2021-12-11 17:26:27 -0500

    more changes to the clues

--- script.js
+++ script.js
@@ -129,155 +129,203 @@ $(document).ready(function () {
 
   let bool1 = false;
   $('.briefcase').click(function () {
-    alert("You have found a briefcase!");
-    if (bool1 == false) {
-      count++;
-      bool1 = true;
-    }
+    if (count < 10) {
+      alert("You have found a briefcase!");
+      if (bool1 == false) {
+        count++;
+        bool1 = true;
+      }
+    } else alert(found10);
   })
   let bool2 = false;
   $('.clock').click(function () {
-    alert("You have found a clock!");
-    if (bool2 == false) {
-      count++;
-      bool2 = true;
-    }
+    if (count < 10) {
+      alert("You have found a clock!");
+      if (bool2 == false) {
+        count++;
+        bool2 = true;
+      }
+    } else alert(found10);
   })
   let bool3 = false;
   $('.clothingonfloor').click(function () {
-    alert("You have found clothing on the floor!");
-    if (bool3 == false) {
-      count++;
-      bool3 = true;
-    }
+    if (count < 10) {
+      alert("You have found clothing on the floor!");
+      if (bool3 == false) {
+        count++;
+        bool3 = true;
+      }
+    } else alert(found10);
   })
   let bool4 = false;
   $('.bat').click(function () {
-    alert("You have found a bat!");
-    if (bool4 == false) {
-      count++;
-      bool4 = true;
-    }
+    if (count < 10) {
+      alert("You have found a bat!");
+      if (bool4 == false) {
+        count++;
+        bool4 = true;
+      }
+    } else alert(found10);
   })
   let bool5 = false;
   $('.footprints').click(function () {
-    alert("You have found footprints!");
-    if (bool5 == false) {
-      count++;
-      bool5 = true;
-    }
+    if (count < 10) {
+      alert("You have found footprints!");
+      if (bool5 == false) {
+        count++;
+        bool5 = true;
+      }
+    } else alert(found10);
   })
   let bool6 = false;
   $('.notes').click(function () {
-    alert("You have found notes!");
-    if (bool6 == false) {
-      count++;
-      bool6 = true;
-    }
+    if (count < 10) {
+      alert("You have found notes!");
+      if (bool6 == false) {
+        count++;
+        bool6 = true;
+      }
+    } else alert(found10);
   })
   let bool7 = false;
   $('.bloodstain2').click(function () {
-    alert("You have found a bloodstain!");
-    if (bool7 == false) {
-      count++;
-      bool7 = true;
-    }
+    if (count < 10) {
+      alert("You have found a bloodstain!");
+      if (bool7 == false) {
+        count++;
+        bool7 = true;
+      }
+    } else alert(found10);
   })
   let bool8 = false;
   $('.fork').click(function () {
-    alert("You have found a fork!");
-    if (bool8 == false) {
-      count++;
-      bool8 = true;
-    }
+    if (count < 10) {
+      alert("You have found a fork!");
+      if (bool8 == false) {
+        count++;
+        bool8 = true;
+      }
+    } else alert(found10);
   })
   let bool9 = false;
   $('.hand').click(function () {
-    alert("You have found a hand!");
-    if (bool9 == false) {
-      count++;
-      bool9 = true;
-    }
+    if (count < 10) {
+      alert("You have found a hand!");
+      if (bool9 == false) {
+        count++;
+        bool9 = true;
+      }
+    } else alert(found10);
   })
   let bool10 = false;
   $('.splatter').click(function () {
-    alert("You have found a splatter!");
-    if (bool10 == false) {
-      count++;
-      bool10 = true;
-    }
+    if (count < 10) {
+      alert("You have found a splatter!");
+      if (bool10 == false) {
+        count++;
+        bool10 = true;
+      }
+    } else alert(found10);
   })
   let bool11 = false;
   $('.knife').click(function () {
-    alert("You have found a knife!");
-    if (bool11 == false) {
-      count++;
-      bool11 = true;
-    }
+    if (count < 10) {
+      alert("You have found a knife!");
+      if (bool11 == false) {
+        count++;
+        bool11 = true;
+      }
+    } else alert(found10);
   })
   let bool12 = false;
   $('.hairbrush').click(function () {
-    alert("You have found a hairbrush!");
-    if (bool12 == false) {
-      count++;
-      bool12 = true;
-    }
+    if (count < 10) {
+      alert("You have found a hairbrush!");
+      if (bool12 == false) {
+        count++;
+        bool12 = true;
+      }
+    } else alert(found10);
   })
   let bool13 = false;
   $('.bloodstain1').click(function () {
-    alert("You have found a bloodtain!");
-    if (bool13 == false) {
-      count++;
-      bool13 = true;
-    }
+    if (count < 10) {
+      alert("You have found a bloodtain!");
+      if (bool13 == false) {
+        count++;
+        bool13 = true;
+      }
+    } else alert(found10);
   })
   let bool14 = false;
   $('.flowerpot').click(function () {
-    alert("You have found a flowerpot!");
-    if (bool14 == false) {
-      count++;
-      bool14 = true;
-    }
+    if (count < 10) {
+      alert("You have found a flowerpot!");
+      if (bool14 == false) {
+        count++;
+        bool14 = true;
+      }
+    } else alert(found10);
   })
   let bool15 = false;
   $('.bloodstain3').click(function () {
-    alert("You have found a bloodstain!");
-    if (bool15 == false) {
-      count++;
-      bool15 = true;
-    }
+    if (count < 10) {
+      alert("You have found a bloodstain!");
+      if (bool15 == false) {
+        count++;
+        bool15 = true;
+      }
+    } else alert(found10);
   })
   let bool16 = false;
   $('.boots').click(function () {
-    alert("You have found a boots!");
-    if (bool16 == false) {
-      count++;
-      bool16 = true;
-    }
+    if (count < 10) {
+      alert("You have found a boots!");
+      if (bool16 == false) {
+        count++;
+        bool16 = true;
+      }
+    } else alert(found10);
   })
   let bool17 = false;
   $('.teddybear').click(function () {
-    alert("You have found a teddy bear!");
-    if (bool17 == false) {
-      count++;
-      bool17 = true;
-    }
+    if (count < 10) {
+      alert("You have found a teddy bear!");
+      if (bool17 == false) {
+        count++;
+        bool17 = true;
+      }
+    } else alert(found10);
   })
   let bool18 = false;
   $('.polish').click(function () {
-    alert("You have found polish!");
-    if (bool18 == false) {
-      count++;
-      bool18 = true;
-    }
+    if (count < 10) {
+      alert("You have found polish!");
+      if (bool18 == false) {
+        count++;
+        bool18 = true;
+      }
+    } else alert(found10);
   })
   let bool19 = false;
   $('.suitcase').click(function () {
-    alert("You have found a suitcase!" + count);
-    if (bool19 == false) {
-      count++;
-      bool19 = true;
-    }
+    if (count < 10) {
+      alert("You have found a suitcase!");
+      if (bool19 == false) {
+        count++;
+        bool19 = true;
+      }
+    } else alert(found10);
+  })
+  let bool20 = false;
+  $('.timenote').click(function () {
+    if (count < 10) {
+      alert("You have found a cryptic note!");
+      if (bool20 == false) {
+        count++;
+        bool20 = true;
+      }
+    } else alert(found10);
   })
 
   // These functions allow the user to flip through the story
